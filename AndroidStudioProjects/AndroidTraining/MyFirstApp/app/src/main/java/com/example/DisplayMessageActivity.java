e2eda2e5b31f711a7e1211469f56fdee07403d6c
muhnurwahid <muhnurwahidazhari@gmail.com>
2017-05-09 22:52:42 +0700

    Display the message

--- AndroidStudioProjects/AndroidTraining/MyFirstApp/app/src/main/java/com/example/DisplayMessageActivity.java
+++ AndroidStudioProjects/AndroidTraining/MyFirstApp/app/src/main/java/com/example/DisplayMessageActivity.java
@@ -1,7 +1,9 @@
 package com.example;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = (TextView) findViewById(R.id.textView2);
+        textView.setText(message);
     }
 }
